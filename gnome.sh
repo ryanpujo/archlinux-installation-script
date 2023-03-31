f0ae263169a8842d4e152152af6f28c1e3c8a599
ryanpujo <pujotriantoro@gmail.com>
2023-03-31 22:22:03 +0700

    adding some font

--- gnome.sh
+++ gnome.sh
@@ -14,7 +14,7 @@ git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-sudo pacman -S xorg gnome gdm gnome-extra gnome-tweaks arc-gtk-theme gst-libav arc-icon-theme firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox 
+sudo pacman -S xorg gnome gdm gnome-extra gnome-tweaks arc-gtk-theme gst-libav arc-icon-theme firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 yay -S jmtpfs visual-studio-code-bin insomnia-bin etcher-bin
 
 sudo systemctl enable gdm
