6e036414123c5e89887ac22dd4a66bc7bbca4617
Ryan Pujo <pujotriantoro@gmail.com>
2023-04-01 00:47:28 +0700

    slight change

--- cinnamon.sh
+++ cinnamon.sh
@@ -17,4 +17,4 @@ makepkg -si
 sudo pacman -Syu xorg cinnamon lightdm xreader xed nemo alacritty gst-libav archlinux-wallpaper firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox dina-font tamsyn-font terminus-font
 yay -S jmtpfs visual-studio-code-bin insomnia-bin etcher-bin ttf-ms-fonts xviewer
 
-sudo systemctl enable gdm
+sudo systemctl enable lightdm
