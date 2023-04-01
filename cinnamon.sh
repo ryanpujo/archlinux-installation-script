fc8dfff2ce2b9872d14e1d7189b5d20bca842876
Ryan Pujo <pujotriantoro@gmail.com>
2023-04-01 21:23:02 +0700

    adding some necessary package to cinnamon installer

--- cinnamon.sh
+++ cinnamon.sh
@@ -14,7 +14,13 @@ git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-sudo pacman -Syu xorg cinnamon sddm xreader xed nemo alacritty gst-libav archlinux-wallpaper firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox dina-font tamsyn-font terminus-font
+sudo pacman -Syu xorg cinnamon sddm p7zip unrar xz nemo-fileroller xreader xed nemo alacritty gst-libav archlinux-wallpaper firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox dina-font tamsyn-font terminus-font
 yay -S jmtpfs visual-studio-code-bin insomnia-bin etcher-bin ttf-ms-fonts xviewer
 
+gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
 sudo systemctl enable sddm
+sudo systemctl enable docker
