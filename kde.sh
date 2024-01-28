72e9bb8a320deadebcd4790bc6fe0744dd8e35f8
ryan pujo <pujotriantoro@gmail.com>
2024-01-28 21:12:47 +0700

    removing qemu-arch-extra and networkmanager-qt package because its no longer available

--- kde.sh
+++ kde.sh
@@ -18,7 +18,7 @@ makepkg -si
 # optional dependency for wine
 # sudo pacman -Syu lib32-giflib lib32-gnutls lib32-v4l-utils lib32-libpulse alsa-plugins lib32-alsa-plugins lib32-alsa-lib lib32-libxcomposite lib32-libxinerama lib32-opencl-icd-loader lib32-gst-plugins-base-libs lib32-sdl2 libgphoto2 sane samba dosbox
 
-sudo pacman -Syu xorg xorg-xinit plasma plasma-wayland-session sddm ark networkmanager-qt kwrite p7zip unrar xz libreoffice gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly gstreamer-vaapi archlinux-wallpaper firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox terminus-font powerline-fonts ttf-font-awesome adobe-source-code-pro-fonts ttf-jetbrains-mono noto-fonts noto-fonts-emoji noto-fonts-extra awesome-terminal-fonts
+sudo pacman -Syu xorg xorg-xinit plasma plasma-wayland-session sddm ark kwrite p7zip unrar xz libreoffice gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-ugly gstreamer-vaapi archlinux-wallpaper firefox vlc papirus-icon-theme mtpfs gvfs-mtp dbeaver docker docker-compose jdk17-openjdk ttf-fira-code go rhythmbox terminus-font powerline-fonts ttf-font-awesome adobe-source-code-pro-fonts ttf-jetbrains-mono noto-fonts noto-fonts-emoji noto-fonts-extra awesome-terminal-fonts
 yay -S jmtpfs visual-studio-code-bin insomnia-bin etcher-bin ttf-ms-fonts ttf-meslo-nerd-font-powerlevel10k
 
 sudo groupadd docker
